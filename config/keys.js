ae37294ea208dd3bae2a6ba17312be6b8da7d4ac
Khoi Nguyen <khoi.ntq@gmail.com>
2018-09-23 16:47:16 -0400

    Updated jwt for register and login routes

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: 'mongodb://khoi:abc.123@ds125068.mlab.com:25068/devconnector_dev'
+  mongoURI: 'mongodb://khoi:abc.123@ds125068.mlab.com:25068/devconnector_dev',
+  secretOrKey: 'secret'
 };
